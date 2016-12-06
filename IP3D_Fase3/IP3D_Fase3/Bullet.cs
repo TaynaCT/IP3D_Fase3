e5342e2acaafb8cc9177ab5062f599bce59f05d6
snowydawolf <snowydawolf@gmail.com>
2016-12-06 10:12:45 +0000

    Bullet

---
+++ IP3D_Fase3/IP3D_Fase3/Bullet.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace IP3D_Fase3
+{
+    class Bullet
+    {
+        Model myBullet;
+        Matrix worldMatrix;
+        Matrix View;
+        Matrix Projection;
+        Camera camera;
+        Map terrain;
+        public Vector2 placement;
+        Vector3 position;
+        public float scale;
+        public float height;
+        public float yaw = 0;
+
+        public Bullet(GraphicsDevice device, ContentManager content, Camera cam, Map map, Vector2 newPlacement)
+        {
+            placement = newPlacement;
+            camera = cam;
+            terrain = map;
+            myBullet = content.Load<Model>("tank");
+
+            //direction
+
+            float aspectRatio = (float)device.Viewport.Width /
+                                      device.Viewport.Height;
+
+
+            height = terrain.SurfaceFollow(placement.X, placement.Y);
+            position = new Vector3(placement.X, height, placement.Y);
+            scale = 0.001f;
+
+            worldMatrix = cam.world;
+            View = cam.view;
+            Projection = cam.projection;
+        }
+
+
+        public void bulletUpdate()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                position += new Vector3(3, 3, 3);
+            }
+        }
+
+
+        public void Draw()
+        {
+            foreach (ModelMesh mesh in myBullet.Meshes) // Desenha o modelo
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.View = camera.view;
+                    effect.Projection = camera.projection;
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
