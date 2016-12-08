ed7fe23284c355b374032f46f7b6613495d3b61b
Tayná Tinôco <a10729@alunos.ipca.pt>
2016-12-08 19:10:49 +0000

    MenuCamera Melhorias1

--- IP3D_Fase3/IP3D_Fase3/MenuCamera.cs
+++ IP3D_Fase3/IP3D_Fase3/MenuCamera.cs
@@ -11,6 +11,7 @@ namespace IP3D_Fase3
 {
     class MenuCamera
     {
+        GraphicsDevice device;
         Matrix projection;
         Matrix view;
         Vector3 position;
@@ -19,38 +20,75 @@ namespace IP3D_Fase3
         CameraSurfaceFollow sfCam;
         CameraThirdPerson tpCam;
 
+        int option;
+
         public MenuCamera(GraphicsDevice device, Vector3 pos)
         {
+            this.device = device;
             position = pos;
-            Select(device);
+            option = 1;
+            Select();
+            SwitchOption();
         }
 
-        public void Select(GraphicsDevice device)
+        //Seleciona o valor de options apartir do input das teclas
+        public void Select()
         {
             //F1 - seleciona camera 3° pessoa
             if (Keyboard.GetState().IsKeyDown(Keys.F1))
             {
-                tpCam = new CameraThirdPerson(device, position);
-                projection = tpCam.projection;
-                view = tpCam.view;
+                option = 1;                
             }
             //F2 - seleciona camera com surface follow
             else if (Keyboard.GetState().IsKeyDown(Keys.F2))
             {
-                sfCam = new CameraSurfaceFollow(device);
-                projection = sfCam.projection;
-                view = sfCam.view;
+                option = 2;                
             }
             //F3 - seleciona camera livre 
             else if (Keyboard.GetState().IsKeyDown(Keys.F3))
             {
-                freeCam = new CameraFree(device);
+                option = 3;                
+            }
+        }
+
+        //seleciona a camera de qacordo com o valor de option
+        public void SwitchOption()
+        {
+            switch (option)
+            {
+                case 1:
+                tpCam = new CameraThirdPerson(device, position);
+                projection = tpCam.projection;
+                view = tpCam.view;
+                break;
+                case 2:
+                    sfCam = new CameraSurfaceFollow(device);
+                projection = sfCam.projection;
+                view = sfCam.view;
+                break;
+                case 3:
+                    freeCam = new CameraFree(device);
                 projection = freeCam.projection;
                 view = freeCam.view;
+                break;
             }
         }
         
-
+        public void Update(Point centre, float timePassed, float surfaceFollow)
+        {
+            switch (option)
+            {
+                case 1:
+                    tpCam.Update(centre, timePassed, surfaceFollow);
+                    break;
+                case 2:
+                    sfCam.Update(centre, timePassed, surfaceFollow);
+                    break;
+                case 3:
+                    freeCam.Update(centre, timePassed);
+                    break;
+            }
+        }
 
         public Matrix Projection
         {
