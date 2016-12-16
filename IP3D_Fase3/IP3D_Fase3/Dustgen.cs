4f2581b6a453ac41b900b9636feb5fa5593d2676
snowydawolf <snowydawolf@gmail.com>
2016-12-16 16:50:27 +0000

    Particulas WIP 2

--- IP3D_Fase3/IP3D_Fase3/Dustgen.cs
+++ IP3D_Fase3/IP3D_Fase3/Dustgen.cs
@@ -14,12 +14,55 @@ namespace IP3D_Fase3
         int particulas;
         float raio, comprimento, largura, altura;
         List<Particle> dust;
-        GraphicsDevice d;
+        GraphicsDevice dev;
         CameraSurfaceFollow cam;
         Random rnd;
 
-        public Dustgen(GraphicsDevice d,CameraSurfaceFollow cam)
+        public Dustgen(GraphicsDevice d,CameraSurfaceFollow c)
         {
+            effect = new BasicEffect(d);
+            effect.VertexColorEnabled = true;
+            particulas = 1000;
+            raio = 6f;
+            comprimento = 6f;
+            largura = 10f;
+            altura = 20f;
+
+            this.cam = c;
+            this.dev = d;
+            rnd = new Random();
+            dust = new List<Particle>();
+        }
+
+        public void ciclo()
+        {
+
+            for (int i = 0; i < 10f; i++)
+                if (dust.Count < particulas)
+                {
+                    dust.Add(new Particle(dev, rnd, raio, altura));
+                }
+                else
+                    break;
+
+            for (int i = 0; i < dust.Count; i++)
+            {
+                dust[i].Update();
+                if (dust[i].Height < 0)
+                {
+                    dust.RemoveAt(i);
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            effect.World = Matrix.Identity;
+            //effect.View = cam.;
+            effect.Projection = cam.projection;
+
+
+
 
         }
     }
