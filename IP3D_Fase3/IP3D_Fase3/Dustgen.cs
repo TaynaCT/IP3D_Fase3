020bf799c2a976c03cccb0be464394f4fe5bc99d
snowydawolf <snowydawolf@gmail.com>
2016-12-16 11:33:33 +0000

    Particulas (WIP)

---
+++ IP3D_Fase3/IP3D_Fase3/Dustgen.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP3D_Fase3
+{
+    class Dustgen
+    {
+        BasicEffect effect;
+        int particulas;
+        float raio, comprimento, largura, altura;
+        List<Particle> dust;
+        GraphicsDevice d;
+        CameraSurfaceFollow cam;
+        Random rnd;
+
+        public Dustgen(GraphicsDevice d,CameraSurfaceFollow cam)
+        {
+
+        }
+    }
+}
--- IP3D_Fase3/IP3D_Fase3/Particle.cs
+++ IP3D_Fase3/IP3D_Fase3/Particle.cs
@@ -13,66 +13,37 @@ namespace IP3D_Fase3
     class Particle
     {
         BasicEffect effect;
-                
-        public VertexPositionColor[] drop;
-        float radius;
-        float distance;
-        //float height;
-        Vector3 fallSpeed;
-        Vector3 position;
-        Vector3 tankPos;
-                
-        public Particle(GraphicsDevice device, Vector3 tankPos, float radius, Random rand)
+        Vector3 posicao, gravidaded;
+        Matrix worldMatrix;
+
+        public Particle(GraphicsDevice d, Random rnd, float raio, float altura)
         {
-            effect = new BasicEffect(device);            
-            effect.VertexColorEnabled = true;
-            this.tankPos = tankPos;
-            //this.height = center.Y;
-            this.radius = radius;
-            distance = (float)rand.NextDouble() * radius;
-            float angle = (float)rand.NextDouble() * MathHelper.TwoPi;
-
-            //pos = c + r * d * (cos(a); 0 ; sin(a)) 
-            float x = distance * (float)Math.Cos(angle);
-            float z = distance * (float)Math.Sin(angle);
-            float y = distance * (float)Math.Tan(angle);
-            //posição inicial da particula
-            position = tankPos - new Vector3(x, y, z);
-
-            // Dois vertices para cada gota
-            drop = new VertexPositionColor[2];
-
-            //direção da queda
-            float fallSpeedX = (float)rand.NextDouble() * .1f;
-            float fallSpeedZ = (float)rand.NextDouble() * .1f;
-            float fallSpeedY = (float)rand.NextDouble() * .1f;
-
-            fallSpeed = new Vector3(fallSpeedX, fallSpeedY, fallSpeedZ);
-            
-            fallSpeed.Normalize();
-
-            drop[0] = new VertexPositionColor(position, Color.Purple);
-            drop[1] = new VertexPositionColor(position, Color.Purple);
-            
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            fallSpeed += new Vector3(.0f, -9.8f, .0f) * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            
-            drop[1].Position = drop[0].Position;
-            drop[0].Position += fallSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            //drop[1].Position += drop[0].Position * (float)gameTime.ElapsedGameTime.TotalSeconds;
-        }
-
-        public void Draw(GraphicsDevice device, Matrix view, Matrix projection)
-        {
-            effect.CurrentTechnique.Passes[0].Apply();
-
+            effect = new BasicEffect(d);
             effect.World = Matrix.Identity;
-            effect.View = view;
-            effect.Projection = projection;
-            device.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineList, drop, 0, 1);
+            worldMatrix = Matrix.Identity;
+
+            posicao = new Vector3((float)rnd.NextDouble() * raio * (float)(Math.Cos(rnd.NextDouble() * MathHelper.TwoPi)),
+                                    altura,
+                                    (float)rnd.NextDouble() * raio * (float)Math.Sin(rnd.NextDouble() * MathHelper.TwoPi));
+
+            gravidaded = new Vector3((float)(rnd.NextDouble() * 0.10) - 0.1f,
+                                                -0.3f,
+                                                (float)(rnd.NextDouble() * 0.10) - 0.1f);
+        }
+
+        public void Update()
+        {
+            posicao += gravidaded;
+        }
+
+        //métodos de retorno
+        public float Height
+        {
+            get { return posicao.Y; }
+        }
+        public Vector3 Pos
+        {
+            get { return posicao; }
         }
 
     }
